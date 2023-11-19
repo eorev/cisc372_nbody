897ae2557518853f212610a889d440c3697713b5
eorev <eorev@udel.edu>
2023-11-18 20:06:53 -0500

    reverted to older version

--- compute.cu
+++ compute.cu
@@ -15,32 +15,41 @@ __global__ void computeAccelerationMatrix(vector3 *accels, vector3 *d_hPos,
   __shared__ double sharedMass[BLOCK_SIZE];
 
   // Load a block of d_hPos and d_mass into shared memory
-  if (threadIdx.y == 0 && j < NUMELEMENTS) {
-    sharedMass[threadIdx.x] = (threadIdx.x < BLOCK_SIZE) ? d_mass[j] : 0.0;
+  if (threadIdx.y == 0 && threadIdx.x < BLOCK_SIZE && j < NUMELEMENTS) {
+    sharedMass[threadIdx.x] = d_mass[j];
   }
 
-  if (i < NUMELEMENTS && j < NUMELEMENTS) {
-    vector3 pos = {d_hPos[j][0], d_hPos[j][1], d_hPos[j][2]};
-    sharedPos[threadIdx.y][threadIdx.x][0] = pos[0];
-    sharedPos[threadIdx.y][threadIdx.x][1] = pos[1];
-    sharedPos[threadIdx.y][threadIdx.x][2] = pos[2];
+  if (i < NUMELEMENTS && j < NUMELEMENTS && threadIdx.x < BLOCK_SIZE &&
+      threadIdx.y < BLOCK_SIZE) {
+    for (int k = 0; k < 3; k++) {
+      sharedPos[threadIdx.y][threadIdx.x][k] = d_hPos[j][k];
+    }
   }
   __syncthreads();
 
   if (i < NUMELEMENTS && j < NUMELEMENTS && i != j) {
     vector3 distance;
-    double magnitude_sq = 0.0;
-    for (int k = 0; k < 3; k++) {
-      distance[k] = sharedPos[threadIdx.y][threadIdx.x][k] - d_hPos[i][k];
-      magnitude_sq += distance[k] * distance[k];
+    if (i < j) { // Calculate only for i < j
+      for (int k = 0; k < 3; k++) {
+        distance[k] = sharedPos[threadIdx.y][threadIdx.x][k] - d_hPos[i][k];
+      }
+
+      double magnitude_sq = distance[0] * distance[0] +
+                            distance[1] * distance[1] +
+                            distance[2] * distance[2];
+      double magnitude = sqrt(magnitude_sq);
+      double accelmag = GRAV_CONSTANT * sharedMass[threadIdx.x] / magnitude_sq;
+
+      // Compute acceleration vector
+      for (int k = 0; k < 3; k++) {
+        double accelComponent = accelmag * distance[k] / magnitude;
+        accels[i * NUMELEMENTS + j][k] = accelComponent;
+        accels[j * NUMELEMENTS + i][k] = -accelComponent; // Using symmetry instead of recomputation
+      }
     }
-
-    double magnitude = sqrt(magnitude_sq);
-    double accelmag = GRAV_CONSTANT * sharedMass[threadIdx.x] / magnitude_sq;
-
+  } else if (i < NUMELEMENTS) {
     for (int k = 0; k < 3; k++) {
-      double accelComponent = accelmag * distance[k] / magnitude;
-      accels[i * NUMELEMENTS + j][k] += accelComponent;
+      accels[i * NUMELEMENTS + j][k] = 0;
     }
   }
 }
@@ -51,13 +60,18 @@ __global__ void updateVelocityPosition(vector3 *accels, vector3 *d_hPos,
 
   if (i < NUMELEMENTS) {
     vector3 totalAccel = {0, 0, 0};
-    for (int j = 0; j < NUMELEMENTS; j++) {
-      for (int k = 0; k < 3; k++) {
+    int j;
+    // Sum accelerations for object i
+    for (j = 0; j < NUMELEMENTS; j++) {
+      int k;
+      #pragma unroll // trying out using a pragma function to increase speed
+      for (k = 0; k < 3; k++)
         totalAccel[k] += accels[i * NUMELEMENTS + j][k];
-      }
     }
 
-    for (int k = 0; k < 3; k++) {
+    // Update velocity and position
+    int k;
+    for (k = 0; k < 3; k++) {
       d_hVel[i][k] += totalAccel[k] * INTERVAL;
       d_hPos[i][k] += d_hVel[i][k] * INTERVAL;
     }
@@ -68,7 +82,7 @@ void compute(vector3 *d_hPos, vector3 *d_hVel, double *d_mass) {
   vector3 *d_accels;
   cudaMalloc((void **)&d_accels, sizeof(vector3) * NUMELEMENTS * NUMELEMENTS);
 
-  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 dimBlock(16, 16);
   dim3 dimGrid((NUMELEMENTS + dimBlock.x - 1) / dimBlock.x,
                (NUMELEMENTS + dimBlock.y - 1) / dimBlock.y);
 
