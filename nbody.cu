db9fce9b990f743e63589cd60290b1c3207dd26c
eorev <eorev@udel.edu>
2023-12-07 14:49:59 -0500

    reverting to older version of nbody

--- nbody.cu
+++ nbody.cu
@@ -1,5 +1,5 @@
-#include "compute.h"
 #include "config.h"
+#include "parallel_compute.h"
 #include "planets.h"
 #include "vector.h"
 #include <math.h>
@@ -7,10 +7,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-// represents the objects in the system.  Global variables
-vector3 *hVel, *d_hVel;
-vector3 *hPos, *d_hPos;
+vector3 *hVel;
+vector3 *hPos;
 double *mass;
+
+vector3 *d_values, **d_accels, *d_hPos, *d_hVel, *d_accel_sum;
+double *d_mass;
+vector3 *temp[NUMENTITIES];
 
 // initHostMemory: Create storage for numObjects entities in our system
 // Parameters: numObjects: number of objects to allocate
@@ -31,6 +34,21 @@ void freeHostMemory() {
   free(hVel);
   free(hPos);
   free(mass);
+}
+
+// freeDeviceMemory: Free storage allocated by a previous call to
+// initDeviceMemory Parameters: None Returns: None Side Effects: Frees the
+// memory allocated to global variables d_values, d_accels, d_hPos, d_hVel,
+// d_accel_sum, and d_mass.
+void freeDeviceMemory() {
+  for (int i = 0; i < NUMENTITIES; i++) {
+    cudaFree(temp[i]);
+  }
+  cudaFree(d_accels);
+  cudaFree(d_hPos);
+  cudaFree(d_hVel);
+  cudaFree(d_accel_sum);
+  cudaFree(d_mass);
 }
 
 // planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an
@@ -97,9 +115,35 @@ int main(int argc, char **argv) {
 #ifdef DEBUG
   printSystem(stdout);
 #endif
+
+  cudaMalloc((void ***)&d_accels, (NUMENTITIES) * sizeof(vector3 *));
+
+  for (int i = 0; i < NUMENTITIES; i++) {
+    cudaMalloc(&temp[i], sizeof(vector3) * NUMENTITIES);
+  }
+  cudaMemcpy(d_accels, temp, sizeof(vector3 *) * NUMENTITIES,
+             cudaMemcpyHostToDevice);
+  cudaMalloc((void **)&d_hPos, (NUMENTITIES) * sizeof(vector3));
+  cudaMalloc((void **)&d_hVel, (NUMENTITIES) * sizeof(vector3));
+  cudaMalloc((void **)&d_accel_sum, (NUMENTITIES) * sizeof(vector3));
+  cudaMalloc((void **)&d_mass, (NUMENTITIES) * sizeof(double));
+
+  cudaMemcpy(d_hPos, hPos, (NUMENTITIES) * sizeof(vector3),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_hVel, hVel, (NUMENTITIES) * sizeof(vector3),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_mass, mass, (NUMENTITIES) * sizeof(double),
+             cudaMemcpyHostToDevice);
+
   for (t_now = 0; t_now < DURATION; t_now += INTERVAL) {
     compute();
   }
+
+  cudaMemcpy(hPos, d_hPos, (NUMENTITIES) * sizeof(vector3),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(hVel, d_hVel, (NUMENTITIES) * sizeof(vector3),
+             cudaMemcpyDeviceToHost);
+
   clock_t t1 = clock() - t0;
 #ifdef DEBUG
   printSystem(stdout);
@@ -107,4 +151,5 @@ int main(int argc, char **argv) {
   printf("This took a total time of %f seconds\n", (double)t1 / CLOCKS_PER_SEC);
 
   freeHostMemory();
+  freeDeviceMemory();
 }
