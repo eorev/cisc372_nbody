4710240a2046b12249bd7f4c5e145292c1aeac0a
eorev <eorev@udel.edu>
2023-12-07 14:46:13 -0500

    linter changes

--- compute.cu
+++ compute.cu
@@ -5,28 +5,20 @@
 #include <math.h>
 #include <stdlib.h>
 
-// Values and Accels are Global Variables
 vector3 *values;
 vector3 **accels;
 
-// The compute functions done parallel
 __global__ void parallelCompute(vector3 *values, vector3 **accels,
                                 vector3 *d_vel, vector3 *d_pos,
                                 double *d_mass) {
 
   int currThreadId = blockIdx.x * blockDim.x + threadIdx.x;
-  int i = currThreadId /
-          NUMENTITIES; // define i in terms of what block/dimension/thread we
-                       // are currently using
-  int j = currThreadId %
-          NUMENTITIES; // define j in terms of what block/dimension/thread we
-                       // are currently using
+  int i = currThreadId / NUMENTITIES;
+  int j = currThreadId % NUMENTITIES;
 
   accels[currThreadId] = &values[currThreadId * NUMENTITIES];
 
-  if (currThreadId <
-      NUMENTITIES * NUMENTITIES) { // Ensure that the result of our block is
-                                   // actually in bounds
+  if (currThreadId < NUMENTITIES * NUMENTITIES) {
     if (i == j) {
       FILL_VECTOR(accels[i][j], 0, 0, 0);
     } else {
@@ -60,10 +52,8 @@ __global__ void parallelCompute(vector3 *values, vector3 **accels,
   }
 }
 
-// Run the three functions that implemenmt parallel design
 void compute() {
 
-  // d_hvel and d_hpos hold the hVel and hPos variables on the GPU
   vector3 *d_vel, *d_pos;
   double *d_mass;
 
@@ -71,7 +61,6 @@ void compute() {
   cudaMallocManaged((void **)&d_pos, (sizeof(vector3) * NUMENTITIES));
   cudaMallocManaged((void **)&d_mass, (sizeof(double) * NUMENTITIES));
 
-  // Copy memory from the host onto the GPU
   cudaMemcpy(d_vel, hVel, sizeof(vector3) * NUMENTITIES,
              cudaMemcpyHostToDevice);
   cudaMemcpy(d_pos, hPos, sizeof(vector3) * NUMENTITIES,
@@ -79,12 +68,10 @@ void compute() {
   cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES,
              cudaMemcpyHostToDevice);
 
-  // Allocate space on the GPU for these variables
   cudaMallocManaged((void **)&values,
                     sizeof(vector3) * NUMENTITIES * NUMENTITIES);
   cudaMallocManaged((void **)&accels, sizeof(vector3 *) * NUMENTITIES);
 
-  // Determine number of blocks that we should be running
   int blockSize = 256;
   int numBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
 
@@ -92,7 +79,6 @@ void compute() {
                                             d_mass);
   cudaDeviceSynchronize();
 
-  // Copy the results back to the device
   cudaMemcpy(hVel, d_vel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
   cudaMemcpy(hPos, d_pos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDefault);
   cudaMemcpy(mass, d_mass, sizeof(double) * NUMENTITIES, cudaMemcpyDefault);
