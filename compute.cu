aec4dd2d2866e640d7db92c864ba00e63cb308dc
eorev <eorev@udel.edu>
2023-12-08 21:29:04 -0500

    Documented code

--- compute.cu
+++ compute.cu
@@ -9,66 +9,95 @@
 extern vector3 *d_values, **d_accels, *d_hPos, *d_hVel, *d_accel_sum;
 extern double *d_mass;
 
-// compute: Updates the positions and velocities of objects in the system based
-// on gravitational forces.
+/**
+ * compute - Main function to update positions and velocities of objects in the
+ * system. This function sets up the execution configuration and launches CUDA
+ * kernels.
+ */
 void compute() {
+  // Calculate grid dimensions for CUDA kernel execution
   int gridDimension = (NUMENTITIES + 7) / 8;
   dim3 gridThreads(8, 8, 3);
   dim3 gridBlocks(gridDimension, gridDimension);
-  calculateAccels<<<gridBlocks, gridThreads>>>(d_accels, d_hPos, d_mass);
+
+  // Launch calcAccels kernel to compute accelerations based on
+  // gravitational forces
+  calcAccels<<<gridBlocks, gridThreads>>>(d_accels, d_hPos, d_mass);
+
+  // Set up dimensions for sumCols kernel
   int threadCount = 64;
   dim3 blockDimension(NUMENTITIES, 3);
   int sharedMemSize = 2 * threadCount * sizeof(double);
-  sumColumns<<<blockDimension, threadCount, sharedMemSize>>>(d_accels,
-                                                             d_accel_sum);
+
+  // Launch sumColumns kernel to sum up acceleration components
+  sumCols<<<blockDimension, threadCount, sharedMemSize>>>(d_accels,
+                                                          d_accel_sum);
+
+  // Set up dimensions for updatePosAndVel kernel
   int positionUpdateBlocks = (NUMENTITIES + 7) / 8;
   dim3 velocityUpdateThreads(8, 3);
-  updatePositionAndVelocity<<<positionUpdateBlocks, velocityUpdateThreads>>>(
+
+  //  Launch updatePositionAndVelocity kernel to update positions and velocities
+  updatePosAndVel<<<positionUpdateBlocks, velocityUpdateThreads>>>(
       d_accel_sum, d_hPos, d_hVel);
 }
 
-__global__ void calculateAccels(vector3 **accels, vector3 *positions,
-                                double *masses) {
-  int entityX = threadIdx.x + blockIdx.x * blockDim.x;
-  int entityY = threadIdx.y + blockIdx.y * blockDim.y;
-  int dimensionZ = threadIdx.z;
-  __shared__ vector3 distComponents[8][8];
-  if (entityX >= NUMENTITIES || entityY >= NUMENTITIES)
+/**
+ * CUDA Kernel Function - updatePosandVel
+ * Updates the positions and velocities of each object based on calculated
+ * accelerations.
+ *
+ * @param totalAccel  Array of total accelerations for each object.
+ * @param positions   Array of object positions.
+ * @param velocities  Array of object velocities.
+ */
+__global__ void updatePosAndVel(vector3 *totalAccel, vector3 *positions,
+                                vector3 *velocities) {
+
+  // Calculate unique index for each thread
+  int entityIdx = threadIdx.x + blockIdx.x * blockDim.x;
+  int dimensionIdx = threadIdx.y;
+
+  // Check for out-of-bound indices
+  if (entityIdx >= NUMENTITIES)
     return;
-  if (entityX == entityY) {
-    accels[entityX][entityY][dimensionZ] = 0;
-  } else {
-    distComponents[threadIdx.x][threadIdx.y][dimensionZ] =
-        positions[entityX][dimensionZ] - positions[entityY][dimensionZ];
-    __syncthreads();
-    double distSquared = distComponents[threadIdx.x][threadIdx.y][0] *
-                             distComponents[threadIdx.x][threadIdx.y][0] +
-                         distComponents[threadIdx.x][threadIdx.y][1] *
-                             distComponents[threadIdx.x][threadIdx.y][1] +
-                         distComponents[threadIdx.x][threadIdx.y][2] *
-                             distComponents[threadIdx.x][threadIdx.y][2];
-    double dist = sqrt(distSquared);
-    double accelMagnitude = -GRAV_CONSTANT * masses[entityY] / distSquared;
-    accels[entityX][entityY][dimensionZ] =
-        accelMagnitude * distComponents[threadIdx.x][threadIdx.y][dimensionZ] /
-        dist;
-  }
+
+  // Update velocities and positions
+  velocities[entityIdx][dimensionIdx] +=
+      totalAccel[entityIdx][dimensionIdx] * INTERVAL;
+  positions[entityIdx][dimensionIdx] +=
+      velocities[entityIdx][dimensionIdx] * INTERVAL;
 }
 
-__global__ void sumColumns(vector3 **accels, vector3 *accelTotals) {
+/**
+ * CUDA Kernel Function - sumCols
+ * Sums up the acceleration components for each object.
+ *
+ * @param accels       2D array of acceleration vectors.
+ * @param accelTotals  Array to store total acceleration for each object.
+ */
+__global__ void sumCols(vector3 **accels, vector3 *accelTotals) {
   int rowIdx = threadIdx.x;
   int colIdx = blockIdx.x;
   int dimension = blockIdx.y;
+
+  // Shared memory for partial sums
   __shared__ int offsetIndex;
   int blockSize = blockDim.x;
   int totalEntities = NUMENTITIES;
   extern __shared__ double sharedArray[];
+
+  // Initialize shared array with acceleration values
   sharedArray[rowIdx] =
       rowIdx < totalEntities ? accels[colIdx][rowIdx][dimension] : 0;
+
+  // Determine offset for partial sums
   if (rowIdx == 0) {
     offsetIndex = blockSize;
   }
   __syncthreads();
+
+  // Sum acceleration components using shared memory
   while (offsetIndex < totalEntities) {
     sharedArray[rowIdx + blockSize] =
         rowIdx + blockSize < totalEntities
@@ -83,6 +112,8 @@ __global__ void sumColumns(vector3 **accels, vector3 *accelTotals) {
     sharedArray[rowIdx] = sumVal;
   }
   __syncthreads();
+
+  // Final reduction to get the total sum
   for (int stride = 1; stride < blockSize; stride *= 2) {
     int arrayIndex = rowIdx * stride * 2;
     if (arrayIndex + stride < blockSize) {
@@ -90,21 +121,56 @@ __global__ void sumColumns(vector3 **accels, vector3 *accelTotals) {
     }
     __syncthreads();
   }
+
+  // Write total sum to accelTotals
   if (rowIdx == 0) {
     accelTotals[colIdx][dimension] = sharedArray[0];
   }
 }
 
-__global__ void updatePositionAndVelocity(vector3 *totalAccel,
-                                          vector3 *positions,
-                                          vector3 *velocities) {
-  int entityIdx = threadIdx.x + blockIdx.x * blockDim.x;
-  int dimensionIdx = threadIdx.y;
-  if (entityIdx >= NUMENTITIES)
+/**
+ * CUDA Kernel Function - calcAccels
+ * Calculates the acceleration of each object due to gravitational forces from
+ * all other objects.
+ *
+ * @param accels    2D array to store calculated accelerations.
+ * @param positions Array of object positions.
+ * @param masses    Array of object masses.
+ */
+__global__ void calcAccels(vector3 **accels, vector3 *positions,
+                           double *masses) {
+  // Calculate unique index for each thread in the kernel
+  int entityX = threadIdx.x + blockIdx.x * blockDim.x;
+  int entityY = threadIdx.y + blockIdx.y * blockDim.y;
+  int dimensionZ = threadIdx.z;
+
+  // Shared memory for distance components
+  __shared__ vector3 distComponents[8][8];
+
+  // Check for out-of-bound indices
+  if (entityX >= NUMENTITIES || entityY >= NUMENTITIES)
     return;
 
-  velocities[entityIdx][dimensionIdx] +=
-      totalAccel[entityIdx][dimensionIdx] * INTERVAL;
-  positions[entityIdx][dimensionIdx] +=
-      velocities[entityIdx][dimensionIdx] * INTERVAL;
+  // Calculate accelerations
+  if (entityX == entityY) {
+    accels[entityX][entityY][dimensionZ] = 0;
+  } else {
+    // Compute distance components between two entities
+    distComponents[threadIdx.x][threadIdx.y][dimensionZ] =
+        positions[entityX][dimensionZ] - positions[entityY][dimensionZ];
+    __syncthreads();
+    // Calculate squared distance and acceleration magnitude
+    double distSquared = distComponents[threadIdx.x][threadIdx.y][0] *
+                             distComponents[threadIdx.x][threadIdx.y][0] +
+                         distComponents[threadIdx.x][threadIdx.y][1] *
+                             distComponents[threadIdx.x][threadIdx.y][1] +
+                         distComponents[threadIdx.x][threadIdx.y][2] *
+                             distComponents[threadIdx.x][threadIdx.y][2];
+    double dist = sqrt(distSquared);
+    double accelMagnitude = -GRAV_CONSTANT * masses[entityY] / distSquared;
+    // Update acceleration vector
+    accels[entityX][entityY][dimensionZ] =
+        accelMagnitude * distComponents[threadIdx.x][threadIdx.y][dimensionZ] /
+        dist;
+  }
 }
--- compute.h
+++ compute.h
@@ -15,22 +15,15 @@ void compute();
 
 // CUDA kernel functions
 
-// mapValuestoAccels: Maps values to acceleration vectors (if used in
-// compute.cu).
-__global__ void mapValuestoAccels(vector3 *values, vector3 **accels);
-
-// fillAccelSum: Fills the acceleration sum array (if used in compute.cu).
-__global__ void fillAccelSum(vector3 *accel_sum);
-
-// calculateAccels: Calculates the accelerations based on positions and masses.
-__global__ void calculateAccels(vector3 **accels, vector3 *hPos, double *mass);
+// calcAccels: Calculates the accelerations based on positions and masses.
+__global__ void calcAccels(vector3 **accels, vector3 *hPos, double *mass);
 
 // sumColumns: Sums the acceleration components.
-__global__ void sumColumns(vector3 **accels, vector3 *accel_sum);
+__global__ void sumCols(vector3 **accels, vector3 *accel_sum);
 
-// updatePositionAndVelocity: Updates the positions and velocities of the
+// updatePosAndVel: Updates the positions and velocities of the
 // entities.
-__global__ void updatePositionAndVelocity(vector3 *accel_sum, vector3 *hPos,
-                                          vector3 *hVel);
+__global__ void updatePosAndVel(vector3 *accel_sum, vector3 *hPos,
+                                vector3 *hVel);
 
 #endif // COMPUTE_H
